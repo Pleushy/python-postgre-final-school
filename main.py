583a6654e0f0d125a9d8d605d8750dff2cc222d2
Ari <109524755+Pleushy@users.noreply.github.com>
2026-06-10 11:20:30 +0200

    fix: spaces to tabs, submit

--- main.py
+++ main.py
@@ -2,33 +2,33 @@ import psycopg
 import ui
  
 class Connection:
-        def __init__(self):
-                self.conn = psycopg.connect(
-                        host="192.168.135.10",
-                        port=5432,
-                        dbname="obce",
-                        user="student",
-                        password="bluemonkey3"
-                )
-                self.cursor = self.conn.cursor()
-        def kill(self):
-                self.cursor.close()
-                self.conn.close()
-        def getCursor(self):
-                return self.cursor
-        def execute(self, req):
-                if req is None: return None
-                self.cursor.execute(req.command, req.query)
-                return self.cursor.fetchall()
+	def __init__(self):
+		self.conn = psycopg.connect(
+			host="192.168.135.10",
+			port=5432,
+			dbname="obce",
+			user="student",
+			password="bluemonkey3"
+		)
+		self.cursor = self.conn.cursor()
+	def kill(self):
+		self.cursor.close()
+		self.conn.close()
+	def getCursor(self):
+		return self.cursor
+	def execute(self, req):
+		if req is None: return None
+		self.cursor.execute(req.command, req.query)
+		return self.cursor.fetchall()
  
 def main():
-        #c = Connection()
-        res = 1
-        while res is not None:
-                req = ui.request()
-                #res = c.execute(req)
-                #if res is not None: ui.submit(res, req.format)
-        #c.kill()
+	#c = Connection()
+	res = 1
+	while res is not None:
+		req = ui.request()
+		#res = c.execute(req)
+		#if res is not None: ui.submit(res, req.format)
+	#c.kill()
  
 if __name__ == "__main__":
-        main()
+	main()
--- ui.py
+++ ui.py
@@ -1,79 +1,81 @@
 from enum import Enum
  
 class Items(Enum):
-        END = "Konec"
-        LIST_OKRES = "Seznam okresu"
-        LIST_OBEC = "Obce v okrese"
-        FIND_OBEC = "Hledat obec"
-        STATS_OKRES = "Statistiky okresu"
+	END = "Konec"
+	LIST_OKRES = "Seznam okresu"
+	LIST_OBEC = "Obce v okrese"
+	FIND_OBEC = "Hledat obec"
+	STATS_OKRES = "Statistiky okresu"
  
 options = [
-        Items.END,
-        Items.LIST_OKRES,
-        Items.LIST_OBEC,
-        Items.FIND_OBEC,
-        Items.STATS_OKRES
+	Items.END,
+	Items.LIST_OKRES,
+	Items.LIST_OBEC,
+	Items.FIND_OBEC,
+	Items.STATS_OKRES
 ]
 
 def submit(data, format):
-        # TODO: replace $ with values in data
-        print(data, format)
+	for chunk in data:
+		for i in chunk:
+			format = format.replace("$", i)
+	print(format)
 
 def getCommand(a):
-        query = None
-        command = None
-        format = None
-        item = options[a]
+	query = None
+	command = None
+	format = None
+	item = options[a]
  
-        if item.name == Items.END.name: return None
-        elif item.name == Items.LIST_OKRES.name:
-                command = "SELECT * FROM okresy;"
-                format = "$"
-        elif item.name == Items.LIST_OBEC.name:
-                query = input("Zadej kod okresu: ")
-                command = "SELECT obce_pob.nazev, SUM(obce_pob.pocet_obyvatel) AS pocet_obyvatel, AVG(obce_pob.prumerny_vek) AS prumerny_vek FROM obce_pob JOIN okresy ON obce_pob.id_okres = okresy.id_okres WHERE obce_pob.id_okres ILIKE (%s) GROUP BY obce_pob.nazev;"
-                format = "$ - $\nPocet obyvatel: $\nPrumerny vek: $"
-        elif item.name == Items.FIND_OBEC.name:
-                query = input("Zadej nazev obce: ")
-                command = "SELECT nazev FROM obce_pob WHERE nazev ILIKE (%s);"
-                format = "$"
-        elif item.name == Items.STATS_OKRES.name:
-                # TODO: finish
-                query = input("Zadej kod okresu: ")
-                command = "SELECT obce_pob.nazev, SUM(obce_pob.pocet_obyvatel) AS pocet_obyvatel, AVG(obce_pob.prumerny_vek) AS prumerny_vek FROM obce_pob JOIN okresy ON obce_pob.id_okres = okresy.id_okres WHERE obce_pob.id_okres ILIKE (%s) GROUP BY obce_pob.nazev;" 
-                format = ""
-        # TODO: add more commands
+	if item.name == Items.END.name: return None
+	elif item.name == Items.LIST_OKRES.name:
+		command = "SELECT * FROM okresy;"
+		format = "$"
+	elif item.name == Items.LIST_OBEC.name:
+		query = input("Zadej kod okresu: ")
+		command = "SELECT obce_pob.nazev, SUM(obce_pob.pocet_obyvatel) AS pocet_obyvatel, AVG(obce_pob.prumerny_vek) AS prumerny_vek FROM obce_pob JOIN okresy ON obce_pob.id_okres = okresy.id_okres WHERE obce_pob.id_okres ILIKE (%s) GROUP BY obce_pob.nazev;"
+		format = "$ - $\nPocet obyvatel: $\nPrumerny vek: $"
+	elif item.name == Items.FIND_OBEC.name:
+		query = input("Zadej nazev obce: ")
+		command = "SELECT nazev FROM obce_pob WHERE nazev ILIKE (%s);"
+		format = "$"
+	elif item.name == Items.STATS_OKRES.name:
+		# TODO: finish
+		query = input("Zadej kod okresu: ")
+		command = "SELECT obce_pob.nazev, SUM(obce_pob.pocet_obyvatel) AS pocet_obyvatel, AVG(obce_pob.prumerny_vek) AS prumerny_vek FROM obce_pob JOIN okresy ON obce_pob.id_okres = okresy.id_okres WHERE obce_pob.id_okres ILIKE (%s) GROUP BY obce_pob.nazev;" 
+		format = ""
+	# TODO: add more commands
 
-        if query is not None:
-                query = query.split(", ")
-                for i in range(len(query)): query[i] = f"%{query[i]}%"
-                query = tuple(query)
+	if query is not None:
+		query = query.split(", ")
+		for i in range(len(query)): query[i] = f"%{query[i]}%"
+		query = tuple(query)
 
-        output = {
-                "command":command,
-                "query":query,
-                "format":format
-        }
+	output = {
+		"command":command,
+		"query":query,
+		"format":format
+	}
 
-        return output
+	return output
  
 def request():
-        print("""
+	print("""
 =========================
  DEMOGRAFIE ČR
 =========================
-        """)
-        a = None
-        while True:
-                for i,v in enumerate(options):
-                        print(f"{i} - {v.value}")
-                a = input("\nVyber: ")
-                try:
-                        a = int(a)
-                        if a < 0 or a > len(options)-1:
-                                raise Exception()
-                except Exception:
-                        print(f"Please input a number between 0 and {len(options)-1}\n")
-                        continue
-                break
-        return getCommand(a)
+	""")
+	a = None
+	while True:
+		for i,v in enumerate(options):
+			print(f"{i} - {v.value}")
+		a = input("\nVyber: ")
+		try:
+			a = int(a)
+			if a < 0 or a > len(options)-1:
+				raise Exception()
+		except Exception:
+			print(f"Please input a number between 0 and {len(options)-1}\n")
+			continue
+		break
+	return getCommand(a)
